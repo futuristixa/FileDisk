ff7cf5c56c13ea2be2532f25d2eefa327cf9fc19
helloleonyin <hello.leonyin@gmail.com>
2015-02-18 18:02:10 -0500

    classes outlined

--- FileOnDisk.java
+++ FileOnDisk.java
@@ -1,5 +1,36 @@
 package proj2;
-
-public class FileOnDisk {
-
+import java.io.File;
+/**
+ * 
+ * @author leonyin
+ *
+ *@parms absPath - absolute path name of a file
+ *@params size - size of a given file
+ */
+public class FileOnDisk implements Comparable<FileOnDisk>{
+String absPath;
+long size;
+	public FileOnDisk(String absPath, long size){
+		this.absPath = absPath;
+		this.size=size;
+	}
+	public long getSize() {
+		return size;
+	}
+	public String getAbsPath() {
+		return absPath;
+	}
+	
+	@Override
+	public int compareTo(FileOnDisk other){
+		if(this.size < other.getSize()){
+			return 1;
+		}
+		else if(this.size == other.getSize()){
+			return 0;
+		}
+		else{
+			return -1;
+		}
+	}
 }
